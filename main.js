aeffccb0fef8c954e94b3c83480b2e0b876c5627
Anshika2412oss <76035744+Anshika2412oss@users.noreply.github.com>
2021-08-18 16:53:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function setup(){
+    canvas= createCanvas(500, 300);
+    canvas.center();
+    video= createCapture(VIDEO);
+    video.hide()
+    classifier= ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/i5qThqqK9//model.json", modelLoaded);
+}
+function draw(){
+    image(video, 0, 0, 500, 300);
+    classifier.classify(video, gotResults);
+}
+function gotResults(error, results){
+    if (error) {
+        console.log(error)
+} else{
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML=results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.tofixed(3)
+}
+}
+function modelLoaded(){
+    console.log('Model Loaded!!')
+}
